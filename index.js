6d867442acf940f95f7c32f13b4c3e71e04d6804
Fco. Javier Martin Carrasco <javray@gmail.com>
2022-01-25 15:01:39 +0100

    try catch finaly

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || '3003'
+const express = require('express');
+const app = express();
+const port = process.env.PORT || '3003';
 
-const cloudscraper = require('cloudscraper')
+const cloudscraper = require('cloudscraper');
 
 app.get('/', async (req, res) => {
+
   if (!req.query.url) {
-    res.send('DOWNER-SC')
+    res.send('DOWNER-SC');
     return;
   }
-  const result = await cloudscraper.get(req.query.url);
-  res.send(result)
-})
+
+  let result = '';
+
+  try {
+    result = await cloudscraper.get(req.query.url);
+  }
+  catch(e) {
+    console.log(e);
+  }
+  finally {
+    res.send(result);
+  }
+
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
