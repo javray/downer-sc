62d2e67299cc78895021385533eee185a13692f2
Fco. Javier Martin Carrasco <javray@gmail.com>
2022-01-25 13:25:35 +0100

    Check URL

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || '3003'
 
-//const cloudscraper = require('cloudscraper')
+const cloudscraper = require('cloudscraper')
 
-app.get('/', (req, res) => {
-  //const result = await cloudscraper.get(req.query.url)
-  //res.send(result)
-  res.send('hola')
+app.get('/', async (req, res) => {
+  if (!req.query.url) {
+    res.send('DOWNER-SC')
+    return;
+  }
+  const result = await cloudscraper.get(req.query.url);
+  res.send(result)
 })
 
 app.listen(port, () => {
